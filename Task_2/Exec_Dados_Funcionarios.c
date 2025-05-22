8bda3599a312b276587937f6c2d846b3e2ce22d1
DaviBrinoLima <davibrinolima@furg.br>
2025-05-22 07:52:50 -0300

    Adicionando Exec Dados Funcionario

---
+++ Task_2/Exec_Dados_Funcionarios.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main(void) {
+    typedef struct Funcionario
+    {
+      int codigo, idade;
+      float salario;
+      char sexo;
+    };
+    
+
+    int cont = 0, flag = 1;
+    Funcionario empresa[200];
+
+
+    while(1) {
+        printf("Insira o"); 
+    }
+}
